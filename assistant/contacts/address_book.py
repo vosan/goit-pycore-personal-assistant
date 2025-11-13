572ddb4bf466cde457af85cd868d342227372104
Mykola <10roman.filippov.email@example.com>
2025-11-13 17:59:50 +0100

    created upcoming

--- assistant/contacts/address_book.py
+++ assistant/contacts/address_book.py
@@ -7,6 +7,35 @@ Responsibilities:
 - Include the `get_upcoming_birthdays()` function.
 """
 
+from datetime import datetime, timedelta
 
 class AddressBook:
     pass
+
+
+
+
+
+
+
+
+def get_upcoming_birthdays(self, days: None):
+        if days == None :
+            user_input = input("Enter the number of days (or press Enter to leave 7): ")
+            if user_input.strip():
+                try: 
+                    days = int(user_input)
+                except ValueError:
+                    print("Please enter an integer")
+                
+        today = datetime.today().date()
+        upcoming = {}
+        for record in self.data.values():
+            if record.birthday:
+                next_bday = record.birthday.value.replace(year=today.year)
+                if next_bday < today:
+                    next_bday = next_bday.replace(year=today.year + 1)
+                delta = (next_bday - today).days
+                if 0 <= delta <= days:
+                    upcoming[record.name.value] = next_bday.strftime("%d.%m.%Y")
+        return upcoming
